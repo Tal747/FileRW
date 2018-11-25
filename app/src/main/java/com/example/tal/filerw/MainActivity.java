479de9098cd6ef20e0403d38a1effa1b62580583
Tal <ty4273@bs.amalnet.k12.il>
2018-11-25 12:46:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/tal/filerw/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.tal.filerw;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class MainActivity extends AppCompatActivity {
+    EditText et1;
+    TextView tv1;
+    String st;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = findViewById(R.id.et1);
+        tv1 = findViewById(R.id.tv1);
+    }
+
+    public void OCbtn1(View view) {
+        String st = et1.getText().toString();
+        FileOutputStream fos = null;
+        try {
+            fos = openFileOutput("user_input.txt", Context.MODE_PRIVATE);
+            OutputStreamWriter osw = new OutputStreamWriter(fos);
+            BufferedWriter bw = new BufferedWriter(osw);
+            try {
+                bw.write(st);
+                Toast.makeText(this, "Data was saved successfully", Toast.LENGTH_SHORT).show();
+                bw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void OCbtn2(View view) {
+        try {
+            InputStream is = openFileInput("user_input.txt");
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            StringBuffer buffer = new StringBuffer();
+            while((st = br.readLine())!=null){
+                buffer.append(st + "\n");
+            }
+            is.close();
+            tv1.setText(buffer);
+            Toast.makeText(this, "Data was read successfully", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e) {
+            Toast.makeText(this, "FileNotFoundException", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        } catch (IOException e) {
+            Toast.makeText(this, "IOException", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+
+    }
+}
